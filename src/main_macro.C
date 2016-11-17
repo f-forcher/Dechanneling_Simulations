fe87002269ccbaace00b3a4235cf8aee78a417b2
fforcher <francesco.forcher@gmail.com>
2016-11-17 14:12:22 +0100

    Added command line options for selecting the crystal

--- src/main_macro.C
+++ src/main_macro.C
@@ -27,6 +27,7 @@
 #include <array>
 #include <cmath>
 #include <cstdio> //snprintf
+#include <string>
 
 #include <TROOT.h>
 #include <TH1.h>
@@ -57,9 +58,24 @@ int main_macro(int argc, char* argv[]) {
 	using namespace std;
 	using mions::DatiSimulazioni;
 
+	auto nomecrys = string( argv[1] );
+
+	/* Example usage:
+	 * ./Optimized_Super/Dechanneling_Simulations STF49 new2
+	 * Analyze STF49 with the second improved simulation
+	 *
+	 * ./Optimized_Super/Dechanneling_Simulations STF49 orig
+	 * Analyze the original simulation for STF49
+	 */
+	auto simvers = (string("orig") == string( argv[2] ) ?
+						string("") :
+						string("_") + string( argv[2] ));
+
 	// Save the directory of the project (remember you are expected to start it from the
 	// top folder of the repo)
 	getcwd( PROJECT_DIR, FILENAME_MAX );
+
+
 
 //	DatiSimulazioni sim_data_test( "crystal_simulations/STF45/run0553/cr_interaction.dat" );
 	//DatiSimulazioni sim_data_test("test/prova_dat");
@@ -81,32 +97,34 @@ int main_macro(int argc, char* argv[]) {
 	};
 
 	char numero_run[FILENAME_MAX];
-	for (const auto& crys : elenco_cristalli_sim) {
-		string crystal_name( crys );
+//	for (const auto& crys : elenco_cristalli_sim) {
 
-		TH1D* histogram5 = nullptr;
-		TH1D* histogram10 = nullptr;
-		TH1D* histogram5_rnd = nullptr;
-		TH1D* histogram10_rnd = nullptr;
+	//Crystal name and simulation version, eg "STF45" or "STF45_new2"
+	string crys_namevers( nomecrys + simvers );
 
-		TH1D* histogramAm = nullptr;
-		TH1D* histogramDech = nullptr;
-		TH1D* histogramCh = nullptr;
-		TH1D* histogramOth = nullptr;
-		TH1D* histogramTot = nullptr;
+	TH1D* histogram5 = nullptr;
+	TH1D* histogram10 = nullptr;
+	TH1D* histogram5_rnd = nullptr;
+	TH1D* histogram10_rnd = nullptr;
 
-		for (auto i = 1; i <= 1000; ++i) {
-			snprintf( numero_run, FILENAME_MAX, "%04d", i ); // print the number of the simulation job to select, with trailing zeroes
-			string nome_file_dat = string( "run" ) + numero_run + "/cr_interaction.dat"; // e.g. run0321/cr_interaction.dat
-			DBG( std::clog << "nome_file_dat: " << nome_file_dat << std::endl
-			; , ; )
-			clog << numero_run << std::endl;
-			mions::read_histograms( crystal_name, nome_file_dat, histogram5, histogram5_rnd, histogram10,
-					histogram10_rnd );
+	TH1D* histogramAm = nullptr;
+	TH1D* histogramDech = nullptr;
+	TH1D* histogramCh = nullptr;
+	TH1D* histogramOth = nullptr;
+	TH1D* histogramTot = nullptr;
 
-			mions::read_histograms_color( crystal_name, nome_file_dat, 10, histogramAm, histogramDech, histogramCh,
-					histogramOth );
-		}
+	for (auto i = 1; i <= 1000; ++i) {
+		snprintf( numero_run, FILENAME_MAX, "%04d", i ); // print the number of the simulation job to select, with trailing zeroes
+		string nome_file_dat = string( "run" ) + numero_run + "/cr_interaction.dat"; // e.g. run0321/cr_interaction.dat
+		DBG( std::clog << "nome_file_dat: " << nome_file_dat << std::endl
+		; , ; )
+		clog << numero_run << std::endl;
+		mions::read_histograms( crys_namevers, nome_file_dat, histogram5, histogram5_rnd, histogram10,
+				histogram10_rnd );
+
+		mions::read_histograms_color( crys_namevers, nome_file_dat, 10, histogramAm, histogramDech, histogramCh,
+				histogramOth );
+	}
 
 //		string canvas_name = "Crystal simulation: " + crystal_name;
 //		auto cSim = new TCanvas( "cRcLd", canvas_name.c_str() );
@@ -116,8 +134,8 @@ int main_macro(int argc, char* argv[]) {
 //		cSim->cd( 2 );
 //		histogram10->Draw();
 
-		string canvas_name_color = "Crystal simulation: " + crystal_name + "color coded";
-		auto cColor = new TCanvas( "cSimColor", canvas_name_color.c_str() );
+	string canvas_name_color = "Crystal simulation: " + crys_namevers + "color coded";
+	auto cColor = new TCanvas( "cSimColor", canvas_name_color.c_str() );
 //		cColor->Divide( 3, 1 );
 //		cColor->cd( 1 );
 //		histogramAm->Draw();
@@ -126,68 +144,68 @@ int main_macro(int argc, char* argv[]) {
 //		cColor->cd( 3 );
 //		histogramCh->Draw();
 
-		histogramTot = new TH1D(
-		/* name */"total_histo",
-		/* title */"Total Histogram",
-		/* X-dimension */600 / 4, -200, 400 );
-		histogramTot->Add( histogramAm );
-		histogramTot->Add( histogramDech );
-		histogramTot->Add( histogramCh );
-		histogramTot->Add( histogramOth );
+	histogramTot = new TH1D(
+	/* name */"total_histo",
+	/* title */"Total Histogram",
+	/* X-dimension */600 / 4, -200, 400 );
+	histogramTot->Add( histogramAm );
+	histogramTot->Add( histogramDech );
+	histogramTot->Add( histogramCh );
+	histogramTot->Add( histogramOth );
 
-		histogramAm->SetFillColorAlpha( kBlue, 0.4 );
-		histogramDech->SetFillColorAlpha( kRed, 0.4 );
-		histogramCh->SetFillColorAlpha( kGreen, 0.4 );
-		histogramOth->SetFillColorAlpha( kMagenta, 0.4 );
-		histogramTot->SetLineColor( kGreen );
+	histogramAm->SetFillColorAlpha( kBlue, 0.4 );
+	histogramDech->SetFillColorAlpha( kRed, 0.4 );
+	histogramCh->SetFillColorAlpha( kGreen, 0.4 );
+	histogramOth->SetFillColorAlpha( kMagenta, 0.4 );
+	histogramTot->SetLineColor( kGreen );
 
-		//Normalize, for confronting with the data
+	//Normalize, for confronting with the data
 //		histogramAm->Scale(1.0/histogramAm->Integral());
 //		histogramCh->Scale(1.0/histogramCh->Integral());
 //		histogramDech->Scale(1.0/histogramDech->Integral());
 //		histogramOth->Scale(1.0/histogramOth->Integral());
 
-		auto totint = histogramAm->Integral() + histogramCh->Integral() + histogramDech->Integral()
-				+ histogramOth->Integral();
-		histogramAm->Scale( 1.0 / totint );
-		histogramCh->Scale( 1.0 / totint );
-		histogramDech->Scale( 1.0 / totint );
-		histogramOth->Scale( 1.0 / totint );
-		histogramTot->Scale( 1.0 / histogramTot->Integral() );
+	auto totint = histogramAm->Integral() + histogramCh->Integral() + histogramDech->Integral()
+			+ histogramOth->Integral();
+	histogramAm->Scale( 1.0 / totint );
+	histogramCh->Scale( 1.0 / totint );
+	histogramDech->Scale( 1.0 / totint );
+	histogramOth->Scale( 1.0 / totint );
+	histogramTot->Scale( 1.0 / histogramTot->Integral() );
 
-		//histogramAm   -> Draw();
-		//histogramCh   -> Draw("same");
-		//histogramDech -> Draw("same");
-		//histogramOth  -> Draw("same");
-		//histogramTot  -> Draw("same");
+	//histogramAm   -> Draw();
+	//histogramCh   -> Draw("same");
+	//histogramDech -> Draw("same");
+	//histogramOth  -> Draw("same");
+	//histogramTot  -> Draw("same");
 
-		// Experimental data
-		string nomefile_dati_sperimentali = "../Old_Macros_Dechanneling/Dechanneling_Histograms.root";
-		TFile * dati_sperimentali = new TFile( nomefile_dati_sperimentali.c_str() );
-		// string nomehistoSper = "hdati10_" + string( crys ); //TODO Generalizzare a un file
-		string nomehistoSper = "hdati10_" + string( "STF45" );
+	// Experimental data
+	string nomefile_dati_sperimentali = "../Old_Macros_Dechanneling/Dechanneling_Histograms.root";
+	TFile * dati_sperimentali = new TFile( nomefile_dati_sperimentali.c_str() );
+	// string nomehistoSper = "hdati10_" + string( crys ); //TODO Generalizzare a un file
+	string nomehistoSper = "hdati10_" + string( "STF45" );
 
-		TH1D * hDatiSper = (TH1D*) dati_sperimentali->Get( nomehistoSper.c_str() );
+	TH1D * hDatiSper = (TH1D*) dati_sperimentali->Get( nomehistoSper.c_str() );
 
 //		histogram10->Scale(1.0/histogram10->Integral());
 //		hDatiSper->Scale(1.0/hDatiSper->Integral());
 
-		histogram10->Scale( 1.0 / histogram10->GetEntries() );
-		histogram10_rnd->Scale( 1.0 / histogram10_rnd->GetEntries() );
-		hDatiSper->Scale( 1.0 / hDatiSper->GetEntries() );
+	histogram10->Scale( 1.0 / histogram10->GetEntries() );
+	histogram10_rnd->Scale( 1.0 / histogram10_rnd->GetEntries() );
+	hDatiSper->Scale( 1.0 / hDatiSper->GetEntries() );
 
-		histogram10->SetLineColor( kGreen );
-		histogram10->Draw();
+	histogram10->SetLineColor( kGreen );
+	histogram10->Draw();
 
-		histogram10_rnd->SetLineColor( kRed );
-		histogram10_rnd->Draw( "same" );
+	histogram10_rnd->SetLineColor( kRed );
+	histogram10_rnd->Draw( "same" );
 
-		hDatiSper->SetLineColor( kRed );
-		hDatiSper->SetMarkerStyle( kPlus );
-		hDatiSper->SetMarkerSize( 0.5 );
-		hDatiSper->Draw( "0 P same" );
+	hDatiSper->SetLineColor( kRed );
+	hDatiSper->SetMarkerStyle( kPlus );
+	hDatiSper->SetMarkerSize( 0.5 );
+	hDatiSper->Draw( "0 P same" );
 
-	}
+//	}
 
 	return 0;
 }
